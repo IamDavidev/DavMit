46260fca36663ff2a7fa0b132aca4eaf5245f00f
IamDavidev <davidlez2mgk@gmail.com>
2023-03-24 12:34:09 -0600

    [ ` Update UI / styles / design · 💄 ` ]  - update colors in cli with methods

---
+++ src/utils/uiCLI.js
@@ -0,0 +1,49 @@
+import chalk from 'chalk';
+
+/**
+ * @param {string} msg
+ * @returns {string}
+ */
+export function printPrimary(msg) {
+  return chalk.magenta.bold(msg);
+}
+
+/**
+ * @param {string} msg
+ * @returns {string}
+ */
+export function printSecondary(msg) {
+  return chalk.yellow.bold(msg);
+}
+
+/**
+ * @param {string} msg
+ * @returns {string}
+ */
+export function printSuccess(msg) {
+  return chalk.green.bold(msg);
+}
+
+/**
+ * @param {string} msg
+ * @returns {string}
+ */
+export function bgPrimary(msg) {
+  return chalk.bgMagenta.bold(msg);
+}
+
+/**
+ * @param {string} msg
+ * @returns {string}
+ */
+export function bgSecondary(msg) {
+  return chalk.bgYellow.bold(msg);
+}
+
+/**
+ * @param {string} msg
+ * @returns {string}
+ */
+export function bgSuccess(msg) {
+  return chalk.bgGreen.bold(msg);
+}
